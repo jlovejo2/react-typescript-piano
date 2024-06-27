e8ec8b0e34b6e69f4474d6ef3a61f59ed0a7dc6c
Eddie Lovejoy <james.lovejoy2@gmail.com>
2024-06-27 09:21:52 -0400

    added generate notes functions

--- src/domain/note.ts
+++ src/domain/note.ts
@@ -68,3 +68,17 @@ export function fromMidi(midi: MidiValue): Note {
 
     return { octave, pitch, index, type, midi}
 }
+
+type NotesGeneratorSettings = {
+    fromNote?: MidiValue
+    toNote?: MidiValue
+}
+
+export function generateNotes({
+    fromNote = LOWER_NOTE,
+    toNote = HIGHER_NOTE
+}: NotesGeneratorSettings = {}): Note[] {
+    return Array(toNote - fromNote + 1).fill(0).map((_, index: number) => fromMidi(fromNote + index));
+}
+
+export const notes = generateNotes();
