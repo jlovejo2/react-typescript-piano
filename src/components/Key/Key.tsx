fd1181ea55926db4f7d029eb2092fbce824b15e9
Eddie Lovejoy <james.lovejoy2@gmail.com>
2024-07-23 09:00:15 -0400

    created onPressObserver hook

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from "react";
 import clsx from "clsx";
 import { NoteType } from "../../domain/note";
 import styles from "../Key/Key.module.css";
+import { usePressObserver } from "../PressObserver";
 
 type PressCallback = () => void
 
@@ -15,12 +16,20 @@ type KeyProps = {
     onDown: PressCallback
 }
 
+
+
 export const Key: FunctionComponent<KeyProps> = ( props ) => {
     const { type, label, onUp, onDown, ...rest } = props;
 
+    const pressed = usePressObserver({
+        watchKey: label,
+        onStartPress: onDown,
+        onFinishPress: onUp
+    })
+
     return (
         <button 
-            className={clsx(styles.key, styles[type])} 
+            className={clsx(styles.key, styles[type], pressed && styles["is-pressed"])} 
             onMouseUp={onUp}
             onMouseDown={onDown}
             type="button"
