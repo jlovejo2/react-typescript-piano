15e8cf325792cb83643b55f9c6552e857e0429e8
Eddie Lovejoy <james.lovejoy2@gmail.com>
2024-10-10 16:27:57 -0400

    create playground and implement with main

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Main } from "./components/Main";
 import { Logo } from "./components/Logo";
 import { Footer } from "./components/Footer";
 import styles from './App.module.css';
@@ -7,7 +8,9 @@ export const App = () => {
   return (
     <div className={styles.app}>
       <Logo />
-      <main className={styles.content} />
+      <main className={styles.content}>
+        <Main />
+      </main>
       <Footer />
     </div>
   );
